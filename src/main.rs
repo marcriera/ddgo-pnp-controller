c1f0aa2a8915582a959397963df7ce10b72dbc33
Marc Riera <marc.riera.irigoyen@gmail.com>
2023-02-05 23:34:55 +0100

    Working evdev tester

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use evdev_rs::Device;
+use evdev_rs::ReadFlag;
+
 fn main() {
-    println!("Hello, world!");
+    let d = Device::new_from_path("/dev/input/event4").unwrap();
+
+    loop {
+        let ev = d.next_event(ReadFlag::NORMAL).map(|val| val.1);
+        match ev {
+            Ok(ev) => println!("Event: time {}.{}, {} {}",
+                              ev.time.tv_sec,
+                              ev.time.tv_usec,
+                              ev.event_type().map(|ev_type| format!("{}", ev_type)).unwrap_or("".to_owned()),
+                              ev.value),
+            Err(_e) => (),
+        }
+    }
 }
