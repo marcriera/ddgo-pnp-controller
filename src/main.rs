974dcce2d7ca7eef29645584ddd1192aa5e21382
Marc Riera <marc.riera.irigoyen@gmail.com>
2023-02-15 23:12:37 +0100

    First version working on an actual PNP unit

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ use evdev_rs::Device;
 use evdev_rs::ReadFlag;
 
 fn main() {
-    let d = Device::new_from_path("/dev/input/event4").unwrap();
+    let d = Device::new_from_path("/dev/input/event1").unwrap();
 
     loop {
         let ev = d.next_event(ReadFlag::NORMAL).map(|val| val.1);
         match ev {
-            Ok(ev) => println!("Event: time {}.{}, {} {}",
-                              ev.time.tv_sec,
-                              ev.time.tv_usec,
+            Ok(ev) => println!("Event: {} {} {}",
                               ev.event_type().map(|ev_type| format!("{}", ev_type)).unwrap_or("".to_owned()),
+                              ev.event_code,
                               ev.value),
             Err(_e) => (),
         }
